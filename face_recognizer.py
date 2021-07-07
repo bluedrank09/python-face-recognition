e1837cb8ba440a4ca9b54de8bf62bcbc8a98e24d
mayababu <mayababu73@gmail.com>
2021-07-07 17:30:07 +1200

    Added image encoding function

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,7 +1,28 @@
-def classify_face(file_name):
-    print(file_name)
-    return True
+import face_recognition as fr
+import os
 
+def classify_face(file_name):
+    try: 
+        faces = get_encoded_faces()
+        print(f"{faces}")
+        return True
+
+    except Exception as error:
+        raise error
+
+def get_encoded_faces():
+    try:
+        encoded = {}
+
+        for dirpath, dirname, filename in os.walk("./faces"):
+            for file in filename : 
+                face = fr.load_image_file("faces/" + file)
+                encoding = fr.face_encodings(face)[0]
+                encoded[file.split(".")[0]] = encoding 
+        return(encoded)
+
+    except Exception as error:
+        raise error
 
 if __name__ == "__main__":
     try:
