4d018b5d8d4f870dc9438a3f2e8ec7bfd11e3f22
mayababu <mayababu73@gmail.com>
2021-07-12 18:25:59 +1200

    Added recognising code and added a rectangle and label.

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,4 +1,5 @@
 import face_recognition as fr
+import numpy as np
 import os
 import cv2
 
@@ -23,8 +24,33 @@ def classify_face(file_name):
         #Encoding all the faces in the image that the user gave.
         unknown_face_encodings = fr.face_encodings(input_faces, face_locations, num_jitters = 50, model = 'small')
 
-        print(f"{unknown_face_encodings}")
-        return True
+        face_names = []
+        for face_encoding in unknown_face_encodings:
+            # See which picture in the library is the best match with the encoded face(s).
+            matches = fr.compare_faces(faces_values, face_encoding)
+            name = "Unknown" 
+
+        face_distances = fr.face_distance(faces_values, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = faces_names[best_match_index]
+
+        face_names.append(name)
+
+        for(top, right, bottom, left), name in zip(face_locations, face_names):
+            #Drawing the box around the face.
+            cv2.rectangle(input_faces, (left-20, top-20), (right+20, bottom+20), (106, 13, 137), 2)
+
+            #Drawing the label for the face in the box
+            cv2.rectangle(input_faces, (left-20, bottom-15), (right+20, bottom+20), (123, 104, 238), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_COMPLEX
+            cv2.putText(input_faces, name, (left-20, bottom+15), font, 1.0, (50,205,50), 1)
+
+        while True:
+            cv2.imshow('Video', input_faces)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return(face_names)
+
 
     except Exception as error:
         raise error
@@ -46,7 +72,7 @@ def get_encoded_faces():
 if __name__ == "__main__":
     try:
         file_name = input("Give me the file name : ")
-        classify_face(file_name)
+        print(classify_face(file_name))
 
     except Exception as error:
         print(error)
