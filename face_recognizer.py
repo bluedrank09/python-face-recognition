09b29df8a5b9f0675bba83cb5ea579dcdefea420
mayababu <mayababu73@gmail.com>
2021-07-16 18:21:13 +1200

    Fixed the code, it can now recognise Andy Samberg, Melissa Fumero, and Stepanie Beatriz.

--- face_recognizer.py
+++ face_recognizer.py
@@ -24,43 +24,48 @@ def classify_face(file_name):
         input_faces = cv2.imread(file_name, cv2.IMREAD_COLOR)
 
         #Finding the face on the image so it knows where to put the box.
-        face_locations = fr.face_locations(input_faces, number_of_times_to_upsample = 3, model='hog' )
+        #face_locations = fr.face_locations(input_faces, number_of_times_to_upsample = 3, model='hog' )
+        face_locations = fr.face_locations(input_faces)
+        log.debug(f"Face locations are : {face_locations}")
 
         #Encoding all the faces in the image that the user gave.
-        unknown_face_encodings = fr.face_encodings(input_faces, face_locations, num_jitters = 50, model = 'small')
+        unknown_face_encodings = fr.face_encodings(input_faces, face_locations, model = 'small')
+        log.debug(f"Unknown faces are : {unknown_face_encodings}")
 
         log.debug(f"Starting face comparisons")
+
         face_names = []
+
         for face_encoding in unknown_face_encodings:
             # See which picture in the library is the best match with the encoded face(s).
             matches = fr.compare_faces(faces_values, face_encoding)
             name = "Unknown" 
-        log.debug("Finished face comparisons")
-        log.info(f"Matches : {matches}")
+            log.debug("Finished face comparisons")
+            log.info(f"Matches : {matches}")
+            face_distances = fr.face_distance(faces_values, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            log.debug(f"Best match index : {best_match_index}, Face names : {faces_names}")
 
-        face_distances = fr.face_distance(faces_values, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        log.debug(f"Best match index : {best_match_index}, Face names : {faces_names}")
-        if matches[best_match_index]:
-            name = faces_names[best_match_index]
-            log.info(f"{inspect.stack()[0][3]} : Name = {name}")
+            if matches[best_match_index]:
+                name = faces_names[best_match_index]
+                log.info(f"{inspect.stack()[0][3]} : Name = {name}")
 
-        face_names.append(name)
-        log.info(f"{inspect.stack()[0][3]} : Face names list = {face_names}")
+            face_names.append(name)
+            log.info(f"{inspect.stack()[0][3]} : Face names list = {face_names}")
 
-        if name != 'Unknown':
+            if name != 'Unknown':
 
-            for(top, right, bottom, left), name in zip(face_locations, face_names):
-                #Drawing the box around the face.
-                cv2.rectangle(input_faces, (left-20, top-20), (right+20, bottom+20), (106, 13, 137), 2)
+                for(top, right, bottom, left), name in zip(face_locations, face_names):
+                    #Drawing the box around the face.
+                    cv2.rectangle(input_faces, (left-20, top-20), (right+20, bottom+20), (106, 13, 137), 2)
 
-                #Drawing the label for the face in the box
-                cv2.rectangle(input_faces, (left-20, bottom-15), (right+20, bottom+20), (123, 104, 238), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_COMPLEX
-                cv2.putText(input_faces, name, (left-20, bottom+15), font, 1.0, (50,205,50), 1)
+                    #Drawing the label for the face in the box
+                    cv2.rectangle(input_faces, (left-20, bottom-10), (right+20, bottom+20), (123, 104, 238), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(input_faces, name, (left-20, bottom+15), font, 0.5, (50,205,50), 1)
 
-        else:
-            print(f"Face not found :(")
+            else:
+                print(f"Face not found :(")
 
         while True:
             cv2.imshow('Video', input_faces)
